d9a0617749bdbec3bfa0f6a8e30cf32108c74459
Zhuoshi Wang <zhuoshi.wang@wur.nl>
2026-05-01 15:00:43 +0200

    3.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 namespace fs = std::filesystem;
 using namespace std;
 
-static const std::string PROGRAM_VERSION = "T2.2.0";
+static const std::string PROGRAM_VERSION = "3.0.0";
 
 static void print_version() {
     std::cout << "MotionDynamics Version: " << PROGRAM_VERSION << std::endl;
